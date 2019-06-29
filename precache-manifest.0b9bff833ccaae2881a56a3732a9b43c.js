d22400742c81f677f46ecb4cf670c8d014e2b1c0
vikas <>
2019-06-29 15:20:09 +0530

    changes

---
+++ precache-manifest.0b9bff833ccaae2881a56a3732a9b43c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fed462ee8ac64cdc09da24712ae5eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89d9a8f49e0af51e1d3c",
+    "url": "/static/css/main.d844e173.chunk.css"
+  },
+  {
+    "revision": "13c0b0702fbfc4ffe0ba",
+    "url": "/static/js/2.8235c69c.chunk.js"
+  },
+  {
+    "revision": "89d9a8f49e0af51e1d3c",
+    "url": "/static/js/main.81743519.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
